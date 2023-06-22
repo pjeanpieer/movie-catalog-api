da07d996a38f7706de4d10baed9ca10ef3c5da35
Pool Aguilar <jeanpieer_aguilar@hotmail.com>
2023-06-22 17:56:31 -0500

    fill routes to able send HTTP request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #good practice api/v1/model_name/action
+  namespace 'api' do
+    namespace 'v1' do
+      get 'read_csv'
+    end
+  end
 end

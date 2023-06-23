cd50e3f25457e68909520a9dc759126d97102fac
Pool Aguilar <jeanpieer_aguilar@hotmail.com>
2023-06-23 05:24:03 -0500

    update resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #good practice api/v1/model_name/action
   namespace 'api' do
     namespace 'v1' do
-      get 'read_csv'
+      resources :movies, only: [:index] do
+        get 'read_csv', on: :collection
+      end
     end
   end
 end
